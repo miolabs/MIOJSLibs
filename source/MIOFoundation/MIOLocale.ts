6174ff617251b0525a3f63901670fc167c30744b
Javier Segura <javier.segura@me.com>
2017-05-26 19:26:57 +0200

    Addeding Formatters

--- source/MIOFoundation/MIOLocale.ts
+++ source/MIOFoundation/MIOLocale.ts
@@ -1,21 +1,16 @@
 /**
  * Created by godshadow on 30/3/16.
  */
-
 /*
 /// <reference path="MIOObject.ts" />
-/// <reference path="MIOWebApplication.ts" />
+
 
 class MIOLocale extends MIOObject
 {
-    constructor()
-    {
-        super();
-    }
-
     public static currentLocale()
     {
-        return MIOWebApplication.sharedInstance().currentLanguage;
+        return "es_ES";
+        //return MIOWebApplication.sharedInstance().currentLanguage;
     }
 }
 */
