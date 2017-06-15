c90f8ff05905f16c06f77a8eb42bb581a27a69e8
Javier Segura <javier.segura@me.com>
2017-06-15 18:05:02 +0200

    merging

--- source/MIOFoundation/MIOURLConnection.ts
+++ source/MIOFoundation/MIOURLConnection.ts
@@ -72,7 +72,7 @@ class MIOURLConnection
                     instance.delegate.connectionDidReceiveData(instance, this.responseText);
                 else if (instance.blockFN != null) {
                     var type = instance.xmlHttpRequest.getResponseHeader('Content-Type');
-                    if( type.substring(0,16) != 'application/json') {
+                    if( type.substring(0,16) != 'application/json' && type.substring(0,9) != 'text/html') {
                         //instance.xmlHttpRequest.overrideMimeType('text/plain; charset=x-user-defined');
                         var filename;
                         if(type == 'application/pdf')
