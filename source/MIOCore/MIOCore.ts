4d574822db2bbee796ca76d404189ed2aba2f9fa
Javier Segura <javier.segura@me.com>
2017-11-10 00:37:25 +0100

    Bug fixing

--- source/MIOCore/MIOCore.ts
+++ source/MIOCore/MIOCore.ts
@@ -5,7 +5,31 @@ function MIOCoreLoadFromFile_async(path:string, target, completion){
     
 }
 
+enum MIOCoreDebugOption {
+    Phone,
+    Pad,
+    Mobile,
+    Desktop
+}
+
+var _MIOCoreDebugOptions = {};
+
+function MIOCoreSetDebugOption(option, value){
+    _MIOCoreDebugOptions[option] = value;    
+
+    switch(option) {
+        case MIOCoreDebugOption.Phone:
+        case MIOCoreDebugOption.Pad:
+            _MIOCoreDebugOptions[MIOCoreDebugOption.Mobile] = value;
+            break;
+    }
+}
+
 function MIOCoreIsPhone(){
+
+    // Debug
+    var value = _MIOCoreDebugOptions[MIOCoreDebugOption.Phone];
+    if (value != null) return value;
 
     var phone = ['iphone','android','blackberry','nokia','opera mini','windows mobile','windows phone','iemobile'];
     for (var i in phone) {
@@ -17,6 +41,10 @@ function MIOCoreIsPhone(){
 }
 
 function MIOCoreIsPad(){
+
+    // Debug
+    var value = _MIOCoreDebugOptions[MIOCoreDebugOption.Pad];
+    if (value != null) return value;
 
     var pad = ['ipad'];
     for (var i in pad) {
@@ -30,22 +58,50 @@ function MIOCoreIsPad(){
 
 function MIOCoreIsMobile()
 {
-   // if (_mc_force_mobile == true)
-   //     return true;
+    // Debug
+    var value = _MIOCoreDebugOptions[MIOCoreDebugOption.Mobile];
+    if (value != null) return value;    
 
-    ///<summary>Detecting whether the browser is a mobile browser or desktop browser</summary>
-    ///<returns>A boolean value indicating whether the browser is a mobile browser or not</returns>
-
-    /*if (sessionStorage .desktop) // desktop storage
-     return false;
-     else if (localStorage.mobile) // mobile storage
-     return true;*/
-
-    // alternative
     //var mobile = ['iphone','ipad','android','blackberry','nokia','opera mini','windows mobile','windows phone','iemobile'];
     var mobile = ['iphone','android','blackberry','nokia','opera mini','windows mobile','windows phone','iemobile'];
     for (var i in mobile) if (navigator.userAgent.toLowerCase().indexOf(mobile[i].toLowerCase()) > 0) return true;
 
     // nothing found.. assume desktop
     return false;
+}
+
+enum MIOCoreAppType
+{
+    Web,
+    iOS,
+    macOS,
+    Android,
+    WindowsMobile,
+    Windows,
+    Linux
+}
+
+var _miocore_app_type:MIOCoreAppType;
+
+function MIOCoreSetAppType(appType:MIOCoreAppType)
+{
+    _miocore_app_type = appType;
+}
+
+function MIOCoreGetAppType():MIOCoreAppType
+{
+    return _miocore_app_type;
+}
+
+function MIOCoreGetMainBundleURL()
+{    
+    var url = null;
+
+    if (MIOCoreGetAppType() == MIOCoreAppType.Web)
+    {
+        url = new MIOURL();    
+        url.initWithURLString(window.location.href);
+    }
+    
+    return url;
 }
