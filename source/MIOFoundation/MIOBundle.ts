43c018ffa375a0e3dba5d275667c55c490e48aa7
Mátyás Budavári <budavariam@gmail.com>
2018-02-21 16:57:05 +0100

    Fix missing MIOCoreWebWorker

--- source/MIOFoundation/MIOBundle.ts
+++ source/MIOFoundation/MIOBundle.ts
@@ -11,20 +11,20 @@ class MIOBundle extends MIOObject
 
     private static _mainBundle = null;
 
-    private _webBundle:MIOBundle_Webworker = null;
+    private _webBundle:MIOCoreBundle = null;
 
     public static mainBundle():MIOBundle
     {
         if (this._mainBundle == null)
         {
             // Main url. Getting from broser window url search field
-            
+
             var url = MIOCoreGetMainBundleURLString();
 
             this._mainBundle = new MIOBundle();
             this._mainBundle.initWithURL(url);
         }
-        
+
         return this._mainBundle;
     }
 
@@ -39,18 +39,18 @@ class MIOBundle extends MIOObject
         {
             if (this._webBundle == null)
             {
-                this._webBundle = new MIOBundle_Webworker();
+                this._webBundle = new MIOCoreBundle();
                 this._webBundle.baseURL = this.url;
             }
 
             this._webBundle.loadHMTLFromPath(path, layerID, this, function(layerData){
-                        
+
                     var domParser = new DOMParser();
                     var items = domParser.parseFromString(layerData, "text/html");
                     var layer = items.getElementById(layerID);
-                            
+
                     //this.localizeSubLayers(layer.childNodes);
-        
+
                     if (target != null && completion != null)
                         completion.call(target, layer);
             });
@@ -67,5 +67,5 @@ class MIOBundle extends MIOObject
         });
     }
 
-    
+
 }
