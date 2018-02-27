8ca41b82281b2c5f177009406dc9a6b365802d55
Mátyás Budavári <budavariam@gmail.com>
2018-02-27 16:38:16 +0100

    Push the generated file, to avoid build errors

--- build/webworkers/Bundle_WebWorker.js
+++ build/webworkers/Bundle_WebWorker.js
@@ -1,59 +1,36 @@
-
 importScripts("../../libs/miocore/index.js");
-importScripts("../../libs/foundation/index.js");
-
-const mySelf = (self as DedicatedWorkerGlobalScope)
-
-
+importScripts("../../libs/miocoreplatformww/index.js");
+var mySelf = self;
 var _languageStrings = null;
-
-mySelf.addEventListener('message', function(e) {
-
+mySelf.addEventListener('message', function (e) {
     var item = e.data;
-
     var cmd = item["CMD"];
-
-    if (cmd == "SetLanguageStrings"){
+    if (cmd == "SetLanguageStrings") {
         _languageStrings = item["LanguageStrings"];
     }
-    else if (cmd == "DownloadHTML")
-    {
+    else if (cmd == "DownloadHTML") {
         var url = item["URL"];
         var layerID = item["LayerID"];
         var path = item["Path"];
-
         downloadHTML(url, layerID, path);
     }
-
 }, false);
-
-
 function downloadHTML(url, layerID, path) {
-
     var instance = this;
-
     var xhr = new XMLHttpRequest();
-    xhr.onload = function(){
-
-        if(this.status == 200 && this.responseText != null)
-        {
-            // success!
+    xhr.onload = function () {
+        if (this.status == 200 && this.responseText != null) {
             instance.parseHTML.call(instance, url, this.responseText, layerID, path);
         }
-        else
-        {
-            // something went wrong
+        else {
             console.log("BUNDLE_WORKER: Error downloading resource at " + url + " (Code: " + this.status + ")");
             instance.parseHTML.call(instance);
         }
     };
-
     console.log("BUNDLE_WORKER: Downloading resource at " + url);
     xhr.open("GET", url);
     xhr.send();
 }
-
-function parseHTML(url, data, layerID, path)
-{
-
+function parseHTML(url, data, layerID, path) {
 }
+//# sourceMappingURL=Bundle_WebWorker.js.map
--- source/MIOCorePlatforms/Web/MIOCoreBundle.ts
+++ source/MIOCorePlatforms/Web/MIOCoreBundle.ts
@@ -13,7 +13,7 @@ class MIOCoreBundle
     {
         if (this._layoutWorker == null)
         {
-            this._layoutWorker = new Worker("libs/miojswebworkers/MIOCoreBundle_WebWorker.js");
+            this._layoutWorker = new Worker("libs/webworkers/Bundle_WebWorker.js");
             // TODO: Set language so we can translate every html file in backgorund
             //this._layoutWorker.postMessage({"CMD" : "SetLanguageStrings", "LanguageStrings" : _MIOLocalizedStrings});
             
