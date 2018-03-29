55f68be3d6f49aa0c3547c241eb594873fb10eed
Mátyás Budavári <budavariam@gmail.com>
2018-03-29 17:34:02 +0200

    Create one file bundle as a result

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -5,7 +5,7 @@ const TARGET = process.env.TARGET ? process.env.TARGET.tolowerCase() : 'webapp';
 const PROD = !!process.env.PROD; // true if defined, false if undefined
 
 const buildPath = path.resolve(__dirname, 'build')
-const targetPath = '../dist/js'
+const targetPath = path.resolve(__dirname, 'dist', 'js')
 
 module.exports = {
   devtool: 'source-map',
@@ -45,26 +45,27 @@ module.exports = {
     path: buildPath
   },
   optimization: {
-    splitChunks: {
-      cacheGroups: {
-        default: false,
-        commons: {
-          // Split code: create a separate "vendor.js" file from the modules imported from "node_modules" folder.
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendor",
-          chunks: "all"
-        }
-      }
-    },
+    // // It is possible to split the code for the 
+    // splitChunks: {
+    //   cacheGroups: {
+    //     default: false,
+    //     commons: {
+    //       // Split code: create a separate "vendor.js" file from the modules imported from "node_modules" folder.
+    //       test: /[\\/]node_modules[\\/]/,
+    //       name: "vendor",
+    //       chunks: "all"
+    //     }
+    //   }
+    // },
     minimize: true // !PROD // for development it might be more suitable
   },
   plugins: [
     new CopyWebpackPlugin(
       [
-        {from: `${buildPath}/*.js*`, to: targetPath, flatten: true}
+        {from: `${buildPath}/*`, to: targetPath, flatten: true}
       ], 
       {
-        debug: 'info'
+        debug: 'debug'
       }
     )
   ],
