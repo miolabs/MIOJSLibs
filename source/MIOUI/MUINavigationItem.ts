e0b80e7e9607aac5d25ad15b16eedce2504ca869
Javier Segura <javier.segura@me.com>
2018-04-20 18:10:36 +0200

    Added title to nav item

--- source/MIOUI/MUINavigationItem.ts
+++ source/MIOUI/MUINavigationItem.ts
@@ -6,6 +6,7 @@ export class MUINavigationItem extends MIOObject
     leftView:MUIView = null;
     titleView:MUIView = null;
     rightView:MUIView = null;
+    title:string = null;
 
     initWithLayer(layer){
         if (layer.childNodes.length > 0) {
@@ -44,7 +45,14 @@ export function MUINavItemSearchInLayer(layer)
 
             if (subLayer.getAttribute("data-nav-item") != null) {
                 let ni:MUINavigationItem = new MUINavigationItem();
-                ni.initWithLayer(subLayer);   
+                ni.initWithLayer(subLayer);  
+                
+                // Check for tittle if center view doesn't exists
+                if (ni.titleView == null) {
+                    let title = subLayer.getAttribute("data-nav-item-title");
+                    if (title != null) ni.title = title;
+                }
+
                 return ni;             
             }
         }
