87f0050664dcc2d410c857b510f4ee50e0116ac4
Javier Segura <javier.segura@me.com>
2018-07-02 16:08:34 +0200

    new changes

--- source/MIORemoteNotificationServer/MIORemoteNotificationServer.ts
+++ source/MIORemoteNotificationServer/MIORemoteNotificationServer.ts
@@ -1,6 +1,58 @@
-import { MIOObject } from "../MIOFoundation";
+import { MIOObject, MIOLog } from "../MIOFoundation";
+import { MIOURL } from "../../../../Libs/MIOJSLibs/source/MIOFoundation";
 
-class MIORemoteNotificationServer extends MIOObject
+export interface MNKWebSocketDelegate {
+    webSocketDidOpenConnection?(webSocket:MNKWebSocket);
+    webSocketDidCloseConnection?(webSocket:MNKWebSocket);
+}
+
+export class MNKWebSocket extends MIOObject
+{
+    static webSocketWithURLString(urlString:string, delegate){
+        let ws = new MNKWebSocket();
+        ws.initWithURL(MIOURL.urlWithString(urlString), delegate);
+
+        return ws;
+    }
+
+    private url:MIOURL = null;
+    private webSocket:WebSocket = null;
+    private delegate = null;
+    
+    initWithURL(url:MIOURL, delegate){
+        this.url = url;
+        this.delegate = delegate;
+
+        let instance = this;
+        this.webSocket = new WebSocket(url.absoluteString);        
+        
+        this.webSocket.onopen = function(ev:Event){
+            instance.openCallback(ev);
+        };
+
+        this.webSocket.onclose = function(ev:Event){
+            instance.closeCallback(ev);
+        };
+
+    }
+
+    private openCallback(ev:Event){
+        if (this.delegate != null && typeof this.delegate.webSocketDidOpen === "function") {
+            this.delegate.webSocketDidOpenConnection(this);
+        }
+    }
+
+    private closeCallback(ev:Event){
+        if (this.delegate != null && typeof this.delegate.webSocketDidClose === "function") {
+            this.delegate.webSocketDidCloseConnection(this);
+        }
+    }
+}
+
+
+
+
+export class MIORemoteNotificationServer extends MIOObject
 {
     private static _sharedInstance:MIORemoteNotificationServer = new MIORemoteNotificationServer();    
     
@@ -16,9 +68,31 @@ class MIORemoteNotificationServer extends MIOObject
         return MIORemoteNotificationServer._sharedInstance;
     }
 
+    private webSocket:MNKWebSocket = null;
+    private webSocketURLString = "ws://dl-rms-dev.eu-central-1.elasticbeanstalk.com";
+    private login(){        
+        if (this.webSocket == null){
+             this.webSocket = MNKWebSocket.webSocketWithURLString(this.webSocketURLString, this);
+        }        
+    }
+
+    webSocketDidOpenConnection(webSocket:MNKWebSocket){
+        MIOLog("Conenction OK");
+    }
+
     private observers = [];
-    addObserver(observer, name:string, context?){
+    addObserver(observer, name:string, userInfo?){
+
+    }
+
+    postNotification(name:string, userInfo?){
+
+    }
+
+    sendRequest(name:string, target?, completion?){
 
     }
 
 }
+
+
